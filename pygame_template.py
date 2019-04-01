043985e6c54f9499e811d3ca4f5bf4a6bf801089
Eric Biersner <eric.biersner@gmail.com>
2019-03-31 17:23:33 -0700

    intitial commit

---
+++ pygame_template.py
@@ -0,0 +1,22 @@
+# Pygame template - skeleton for a new pygame project
+
+
+
+
+
+
+# Game Loop
+running = True
+while running:
+    # keep loop running at the right speed
+    #### Process input (events)
+
+    #### update
+
+
+    #### draw / render
+ # for double buffering
+
+
+
+pygame.quit()
---
+++ settings.py
@@ -0,0 +1,24 @@
+# game options and settings
+TITLE = "Jumpy"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12 # smaller means faster max speed but take longer to come to stop
+PLAYER_GRAV = 0.8
+
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                (WIDTH/2 - 50, HEIGHT * 3/4, 100, 20),
+                (125, HEIGHT - 350, 100, 20),
+                (350, 200, 100, 20),
+                (175, 100, 50, 20)]
+# define colors RGB
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0, 255,0)
+BLUE = (0,0,255)
+YELLOW = (255, 255, 0)
