b42bc0d86239dc1ae2a889281657d65fdfe98b0a
MrEricB <eric.biersner@gmail.com>
2019-04-02 18:47:24 -0700

    added music and sound effects, fixed jump

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # Jumpy platform game
+
+# Art from Kenny.nl
+# Happy Tune by http://oepngameart.org/users/syncopika
+# Yippe by http://opengameart.org/users/snabisch
+
+
 import pygame as pg
 import random
 from settings import *
@@ -18,9 +24,9 @@ class Game:
 
     def load_data(self):
         # load the high score
-        # self.dir = path.dirname(__file__)
         self.dir = path.dirname(path.realpath(__file__))
-        print("dir: ", self.dir)
+
+        #load high score
         with open(path.join(self.dir, HS_FILE),'r+') as f:
             try:
                 # there is a high score in the file
@@ -30,6 +36,15 @@ class Game:
                 print("can not read file")
                 self.highscore = 0
 
+        # load spritesheet 
+        img_dir = path.join(self.dir, 'img')
+        self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET))
+        # load sounds and music
+        self.snd_dir = path.join(self.dir, 'snd')
+        self.jump_sound = pg.mixer.Sound(path.join(self.snd_dir, 'Jump3.wav'))
+
+        
+
     def new(self):
         # start a new game
         self.score = 0
@@ -38,19 +53,23 @@ class Game:
         self.player = Player(self) # self inside () is a reference to the game
         self.all_sprites.add(self.player)
         for plat in PLATFORM_LIST:
-            p = Platform(*plat)
+            p = Platform(self, *plat)
             self.all_sprites.add(p)
             self.platforms.add(p)
+        # load background music
+        pg.mixer.music.load(path.join(self.snd_dir, 'happy_tune.ogg'))
         self.run()
     
     def run(self):
         # game loop
+        pg.mixer.music.play(loops=-1)
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
             self.events()
             self.update()
             self.draw()
+        pg.mixer.music.fadeout(500) # can use stop() but this it is a hard stop, not a fade
 
     def update(self):
         # game loop - update
@@ -59,13 +78,19 @@ class Game:
         if self.player.vel.y > 0:
             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
             if hits:
-                self.player.pos.y = hits[0].rect.top
-                self.player.vel.y = 0
+                lowest = hits[0]
+                for hit in hits:
+                    if hit.rect.bottom > lowest.rect.bottom:
+                        lowest = hit
+                if self.player.pos.y < lowest.rect.centery: #how high into platform you need to jump to get on top of the platform
+                    self.player.pos.y = lowest.rect.top
+                    self.player.vel.y = 0
+                    self.player.jumping = False
         # if player reaches top 1/4 of screen
         if self.player.rect.top <= HEIGHT/4:
-            self.player.pos.y += abs(self.player.vel.y) # camera movment same as player speed, to keep smooth camera movment
+            self.player.pos.y += max(abs(self.player.vel.y), 2) # camera movment same as player speed, to keep smooth camera movment
             for plat in self.platforms:
-                plat.rect.y += abs(self.player.vel.y)
+                plat.rect.y += max(abs(self.player.vel.y),2)
                 if plat.rect.top >= HEIGHT:
                     # delete any platforms that are not on the screen
                     plat.kill()
@@ -82,9 +107,8 @@ class Game:
         # spawn new platforms to keep the same average number of platforms
         while len(self.platforms) < 6:
             width = random.randrange(50, 100)
-            p = Platform(random.randrange(0, WIDTH-width), 
-                        random.randrange(-75, -30),
-                        width, 20)
+            p = Platform(self, random.randrange(0, WIDTH-width), 
+                        random.randrange(-75, -30))
             self.platforms.add(p)
             self.all_sprites.add(p)
 
@@ -99,17 +123,23 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.player.jump()
+            if event.type == pg.KEYUP: # if not holding keydonw jump is not as high, so hold down for hight jump
+                if event.key == pg.K_SPACE:
+                    self.player.jump_cut()
 
     def draw(self):
         #game loop draw
         self.screen.fill(BGCOLOR)
         self.all_sprites.draw(self.screen)
+        self.screen.blit(self.player.image, self.player.rect)
         self.draw_text(str(self.score), 22, WHITE, WIDTH/2, 15)
         # after drawing everying, flip the display
         pg.display.flip()
 
     def show_start_screen(self):
         # game splash/start screen
+        pg.mixer.music.load(path.join(self.snd_dir, 'Yippee.ogg'))
+        pg.mixer.music.play(loops=-1)
         self.screen.fill(BGCOLOR)
         self.draw_text(TITLE, 40, WHITE, WIDTH/2, HEIGHT/4)
         self.draw_text("Arrows to move, Space to jump", 22, WHITE, WIDTH/2, HEIGHT/2)
@@ -117,11 +147,14 @@ class Game:
         self.draw_text(f"High score: {str(self.highscore)}", 22, WHITE, WIDTH/2, 15)
         pg.display.flip()
         self.wait_for_key()
+        pg.mixer.music.fadeout(500)
 
     def show_go_screen(self):
         # game over/continue screen
         if not self.running: # this is so the X closes the game window when playing instead of going to the game over screen
             return
+        pg.mixer.music.load(path.join(self.snd_dir, 'Yippee.ogg'))
+        pg.mixer.music.play(loops=-1)
         self.screen.fill(BGCOLOR)
         self.draw_text("GAME OVER", 40, WHITE, WIDTH/2, HEIGHT/4)
         self.draw_text(f"SCORE: {str(self.score)}", 22, WHITE, WIDTH/2, HEIGHT/2)
@@ -136,6 +169,7 @@ class Game:
 
         pg.display.flip()
         self.wait_for_key()
+        pg.mixer.music.fadeout(500)
 
     def wait_for_key(self):
         waiting = True
